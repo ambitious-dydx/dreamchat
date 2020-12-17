5b0341d3e801c68d5d705adae7b386514708a622
ambitious-dydx <amosdaniel290@gmail.com>
2020-12-17 04:13:57 +0100

    dreamchat

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var routes=require('./routes');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var sockets=require('./routes/sockets.js');
+var app = express();
+
+var server=require('http').createServer(app);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/chatroom', routes.chatroom);
+app.get('/rooms', routes.rooms);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+var io=require('socket.io')(server);
+
+
+server.listen(process.env.PORT||3090,function(){
+  console.log("Express server listening on port  process.env.port or " + 3090); 
+});
+
+sockets.initialize(io);
